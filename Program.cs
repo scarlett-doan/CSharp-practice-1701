504cb1cb32eab43614df5fe8e6ce90224c423600
Scarlett Doan <scarlettdoan@Scarletts-Air.home>
2023-01-18 01:12:01 +0200

    C# Assignment 1701

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿#pragma warning disable
+using System;
 using System.Globalization;
+using System.Diagnostics;
 
 namespace CSharpFundamental
 {
@@ -15,16 +17,51 @@ namespace CSharpFundamental
             /* Challenge 2 */
             int[,] arrayA = { { 3, 5, 4, 6 }, { 3, 7, 8, 3 } };
             int[,] arrayB = { { 5, 1 }, { 8, 4 }, { 2, 9 }, { 2, 3 } };
+            
             int[,] result = matrixMultiply(arrayA, arrayB);
-            Console.WriteLine(result); //share your findings to Slack
+            PrintMatrix(result);
+            
         }
         static string toTitleCase(string input)
         {
-            /* Write your code here */
+            string[] tmpStr = input.Split(' ');
+            string result = "";
+
+            foreach (var item in tmpStr)
+            {
+                result +=  Char.ToUpper(item[0]) + item.Substring(1) + ' ';
+            }
+            return result.TrimEnd();
         }
         static int[,] matrixMultiply(int[,] array1, int[,] array2)
         {
-            /* Write your code here */
+            var result = new int[array1.GetLength(0),array2.GetLength(1)];
+            
+            for (int i = 0; i < array1.GetLength(0); i++)
+            {
+                for (int j = 0; j < array2.GetLength(1); j++)
+                {
+                    int temp = 0;
+                    for (int k = 0; k < array1.GetLength(1); k++)
+                    {
+                        temp += array1[i, k] * array2[k, j];
+                    }
+                    result[i, j] = temp;
+                }
+            }
+            return result;
+        }
+
+        static void PrintMatrix(int[,] matrix)
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write("{0} ", matrix[i, j]);
+                }
+                Console.Write(Environment.NewLine);
+            }
         }
     }
 }
